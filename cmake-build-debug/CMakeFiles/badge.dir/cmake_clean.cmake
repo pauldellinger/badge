977313191882e081a4ef7e193668a01458e00bde
pauldellinger <pd88@duke.edu>
2021-10-30 10:47:12 -0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/badge.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/badge.dir/main.cpp.obj"
+  "badge.exe"
+  "badge.exe.manifest"
+  "badge.pdb"
+  "libbadge.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/badge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
