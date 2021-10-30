d1f7ae56cdf51d91ef2a70c4ce0c0d6f77c5e607
Jordan Burton <jordieburton3@gmail.com>
2021-10-30 11:11:44 -0400

    mouse movement

--- main.cpp
+++ main.cpp
@@ -1,14 +1,39 @@
-#include <iostream>
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
-{
+//bool isMouseOver(sf::Shape &shape, const sf::Input &input)
+//{
+//    sf::Vector2f mousePos(input.GetMouseX(), input.GetMouseY());
+//
+//    if(
+//        // check mouse position
+//            (mousePos.x >= shape.GetPosition().x)
+//            &&
+//            (mousePos.x <= shape.GetPosition().x + shape.GetSubRect().GetWidth())
+//            &&
+//            (mousePos.y >= shape.GetPosition().y)
+//            &&
+//            (mousePos.y <= shape.GetPosition().y + shape.GetSubRect().GetHeight())
+//            &&
+//            // check pixel on shape
+//            (shape.GetPixel(mousePos.x - shape.GetPosition().x, mousePos.y - shape.GetPosition().y).a == 255)
+//            )
+//        return true;
+//    return false;
+//}
+
+int main() {
+    sf::CircleShape shape(50);
+    // set the shape color to green
+    shape.setFillColor(sf::Color(100, 250, 50));
     // create the window
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
     // run the program as long as the window is open
     while (window.isOpen())
     {
+        sf::Mouse mouse;
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
@@ -22,11 +47,15 @@ int main()
         window.clear(sf::Color::Black);
 
         // draw everything here...
-        // window.draw(...);
+        window.draw(shape);
+
+        shape.setPosition(
+                mouse.getPosition(window).x - shape.getRadius(),
+                mouse.getPosition(window).y - shape.getRadius()
+        );
 
         // end the current frame
         window.display();
     }
-
     return 0;
 }
